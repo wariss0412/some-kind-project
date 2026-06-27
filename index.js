093c014c8c64c0d2bf1b68a3b6cfcea92ef5d950
wariss0412 <wariss0412@gmail.com>
2026-06-27 14:47:30 +0700

    work complete

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 //ดึงโคด้ของ express เข้ามาใน java
 const express = require('express')
+const cors = require('cors')
 
 //สร้าง server
 const app = express()
-
+app.use(express.json())
+app.use(cors())
 //บอก server ฟังportอะไร
 app.listen(3000, () => {
     console.log("Server starts successfully")
@@ -11,7 +13,32 @@ app.listen(3000, () => {
 
 //สร้างApi ที่ parth
 app.get('/', (req, res) =>{
+    const A = parseFloat(req.query.A)
+    const B = parseFloat(req.query.B)
+    const ope = req.query.ope
+
+    if (isNaN(A) || isNaN(B)){
+        return res.json("error: Invalid number")
+    }else if (ope == '+'){
+        result = A+B
+    }else if (ope === '-') {
+        result = A - B
+    } else if (ope === '*') {
+        result = A * B
+    } else if (ope === 'x') {
+        result = A * B
+    } else if (ope === '%') {
+        result = B !== 0 ? A % B : "It a zero can't divide"
+    } else if (ope === '/') {
+        result = B !== 0 ? A / B : "It a zero can't divide" 
+    } else { result = "error : You need some operator or correct operater" 
+
+    }
+   
+        
+    
     res.json({
-        "message": "Hello World"
+        
+        "result": result
     })
 })
